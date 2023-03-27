43f9d1877f1206da4ccf7181e1077f93c6190b4f
amir <shiodiyev.amir123@example.com>
2023-03-27 10:43:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1
+let number = prompt('1:Напишите число')
+
+if(number % 2==0 ){
+    alert( "Четное")
+}
+else{ alert( "Нечетное")}
+
+
+// 2
+let name = prompt('2:Как вас зовут')
+
+
+
+let age = prompt('Сколько вам лет')
+if(age  >= 18 ){
+    alert("Заходи")
+}
+else{ alert("Еще рано")}
+
+
+
+
+
